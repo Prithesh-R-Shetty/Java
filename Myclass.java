9824d612eb1f08ebd6b7ab33b82f2350d71385e9
Prithesh-R-Shetty <prithesh.23cs104@sode-edu.in>
2024-12-24 14:03:13 +0530

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,8 @@
+package Mypack;
+
+public class Myclass {
+    public void displaymessage() {
+        System.out.println("This msg is from MyPackage class within pkg2");
+    }
+}
+
---
+++ TestPack.java
@@ -0,0 +1,8 @@
+import Mypack.Myclass;
+public class TestPack{
+public static void main(String[] args) {
+
+    Myclass  obj=new Myclass();
+   obj.displaymessage();
+}
+}
